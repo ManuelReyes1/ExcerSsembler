f99b6b079be67b572ed61399bbac69ba3b4926e3
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-02 15:58:23 -0800

    Código solo

---
+++ Capitulo2/puntero.s
@@ -0,0 +1,20 @@
+/*
+*
+*
+*
+*
+*/
+.data
+var1:  .word 3
+/*el puntero toma la var1*/
+puntero_var1: .word var1
+
+.text
+.global main
+
+main: ldr r0, =puntero_var1
+
+  ldr r1, [r0]
+  ldr r2, [r1]
+  ldr r3, =var1
+  bx  lr
