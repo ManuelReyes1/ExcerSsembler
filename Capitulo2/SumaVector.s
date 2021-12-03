4ed913b33496b76023c1049b48cf2740882a1e5b
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-03 10:24:36 -0800

    comentarios

--- Capitulo2/SumaVector.s
+++ Capitulo2/SumaVector.s
@@ -10,18 +10,16 @@ var1: .asciz " La suma es %d \n"
 var2: .word 128, 32, 100, - 30, 124
 .text
 .global main
-  /* Salvamos registros */
+  
 main: push {r4, lr}
-  /* Inicializamos variables y apuntamos r2 a var2 */
     mov r0, #5
     mov r1, #0
-    ldr r2, =var2
+    ldr r2, =var2 //le asignamos r2 a var2
   /* Bucle que hace la suma */
 bucle : ldr r3, [r2] , #4
     add r1, r1, r3
     subs r0, r0, #1
-    bne bucle
-/* Imprimimos resultado */
+    bne bucle //llamamos de nuevo al bucle
     ldr r0, = var1
     bl printf
 /* Recuperamos registros y salimos */
