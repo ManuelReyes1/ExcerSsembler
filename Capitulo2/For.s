fd37b4f9999964059a20097f694bdbc3dec816f4
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-03 10:31:51 -0800

    Update For.s

--- Capitulo2/For.s
+++ Capitulo2/For.s
@@ -3,24 +3,21 @@
 * Autor: Rodriguez Moreno Omar Gamaliel
 * 06/12/2021
 */
-
-  .global main
+.data
+var1:   .asciz " %d\012 \000"
+.text
+    .global main
 main:
-  push {r4, r5, r6, lr}
-  mov r4, #100
-  ldr r5, .L5
-.LC0
-  .ascii " %d\012 \000"
-.L2
-  mov r1, r4
-  mov r0, r5 
-  bl  printf
-  subs  r4, r4 #2
-  bne .L2
-  mov r0, r4
-  pop {r4, r5, r6, pc}
-.L6
-  .align 2
-.L5
-  .word .LC0
-  .section  .rodata.str1.1
+  push {r4, lr}
+  mov r4, #100 //empezamos en 100 asignado a r4
+
+bfor:
+    mov r1, r4 //r1 guarda el valor de r4
+    mov r0, =var1
+    bl  printf
+    subs  r4, r4 #2 //r4 ahora vale r4 -2
+    bne bfor
+    mov r0, r4
+    pop {r4, lr}
+    pop {r4, pc}
+    bx  lr
