c875f03bc0fa59267fc2d6db7ba3571ac19b4ebb
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-02 18:39:03 -0800

    código

--- Capitulo2/EjemploWhile.s
+++ Capitulo2/EjemploWhile.s
@@ -1,23 +1,24 @@
-/*
-*/
-  .global main
+       .global main
 
 main:
-  ldr r1, =vi
-  ldr r1, [r1]
-  ldr r2, =vf
-  ldr r2, [r2]
-.L2:
-  .word .LC0
-  .section  .rodata.str1.1
-.LC0:
-  .ascii "Hola mundo\000"  
-bucle:  cmp r1, r2
-  bhi salir
-  /* Cuerpo
-  del
-  bucle */
-  ldr r0, .L2
-  add r1, r1, #1
-  b bucle
-salir:
+        push    {r4, r5, r6, lr}
+        mov     r4, #0
+        ldr     r5, .L7
+bfor:
+        mov     r1, r4
+        mov     r0, r5
+        add     r4, r4, #1
+        bl      printf
+        cmp     r4, #6
+        bne     bfor
+        ldr     r5, .L7
+        mov     r4, #0
+bwhile:
+        mov     r1, r4
+        mov     r0, r5
+        add     r4, r4, #1
+        bl      printf
+        cmp     r4, #6
+        bne     bwhile
+        mov     r0, #0
+        pop     {r4, r5, r6, pc}
