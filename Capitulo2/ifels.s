790fdbe3d77316a8535ede3378c481bfae5d6f8f
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-02 19:13:34 -0800

    Create ifels.s

---
+++ Capitulo2/ifels.s
@@ -0,0 +1,16 @@
+        .global main
+
+main:
+        push    {r4, lr}
+        ldr     r0, .L5
+        bl      printf
+        mov     r0, #0
+        pop     {r4, pc}
+.L5:
+        .word   .LC0
+
+        .section        .rodata.str1.1,"aMS",%progbits,1
+.LC0:
+        .ascii  "Si\000"
+.LC1:
+        .ascii  "No \000"
