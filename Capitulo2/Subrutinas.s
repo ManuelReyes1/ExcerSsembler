85bdef951281e09d2d637437bae32094ba562bcd
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-02 16:17:25 -0800

    Comentarios y datos

--- Capitulo2/Subrutinas.s
+++ Capitulo2/Subrutinas.s
@@ -1,6 +1,7 @@
-/*
-
-
+/*Instito tecnológico de Tijuana
+* Lenguaje de interfaz
+* Autor: Rodriguez Moreno Omar Gamaliel
+* 06/12/2021
 */
   .global main
   
@@ -8,10 +9,12 @@ main: mov r1, #1
   mov r2, #2
   bl nivel1
   mov r5, #5 
-  
+     /*Para permitir más niveles de subrutinas sin petarlo
+       almacena en pila con push y pop lr*/
 nivel1: push  {lr}
   mov r3, #3
   bl nivel2
+  /*manejo en pila de lr*/
   pop {lr}
   bx lr
 nivel2: mov r4, #4
