f312673deeb9995fc168cc110c51a1a0b4fa8eb6
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-02 17:23:15 -0800

    bucle

--- Capitulo2/EjemploWhile.s
+++ Capitulo2/EjemploWhile.s
@@ -7,12 +7,17 @@ main:
   ldr r1, [r1]
   ldr r2, =vf
   ldr r2, [r2]
-  
+.L2:
+  .word .LC0
+  .section  .rodata.str1.1
+.LC0:
+  .ascii "Hola mundo\000"  
 bucle:  cmp r1, r2
   bhi salir
   /* Cuerpo
   del
   bucle */
+  ldr r0, .L2
   add r1, r1, #1
   b bucle
 salir:
