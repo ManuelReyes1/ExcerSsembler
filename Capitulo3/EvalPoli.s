e6b3d5fb9b29c393c502102ee054b5a9fc74a48d
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-06 15:25:13 -0800

    Update EvalPoli.s

--- Capitulo3/EvalPoli.s
+++ Capitulo3/EvalPoli.s
@@ -1,60 +1,57 @@
 .data
-var1 : .asciz " %d\ n"
+var1:.asciz" %d\ n"
 .text
 .global main
 
-main : push { r4, lr }
+main:push{r4,lr}
 
-mov r0, # 1
-mov r1, # 2
-mov r2, # 3
-mov r3, # 4
+movr0, #1
+movr1, #2
+movr2, #3
+movr3, #4
 
-mov r4, # 5
-push { r4 }
+movr4, #5
+push{r4}
 
-bl poly3
+blpoly3
 
-add sp, # 4
+addsp, #4
 
-mov r1, r0
-ldr r0, = var1
-bl printf
+movr1, r0
+ldrr0, =var1
+blprintf
 
-mov r0, # 1
-mov r1, # -1
-mov r2, # 1
-mov r3, # -1
-mov r4, # 8
-push { r4 }
-bl poly3
-add sp, # 4
-
-mov r1, r0
-ldr r0, = var1
-bl printf
-
-mov r0, # 2
-mov r1, # 0
-mov r2, # 0
-mov r3, # 0
-mov r4, # 8
-push { r4 }
-bl poly3
-add sp, # 4
-mov r1, r0
-ldr r0, = var1
-bl printf
-
-pop { r4, lr }
-bx lr
-.equ param5, 4 *1 
-poly3 : push { r4 }
-ldr r4, [ sp, # param5 ] 
-smlabb r3, r2, r4, r3 
-smulbb r2, r4, r4 
-smlabb r3, r1, r2, r3 
-smulbb r2, r2, r4
-smlabb r0, r0, r2, r3 
-pop { r4 }
-bx lr 
+movr0, #1
+movr1, #-1
+movr2, #1
+movr3, #-1
+movr4, #8
+push{r4}
+blpoly3
+addsp, #4
+movr1, r0
+ldrr0, =var1
+blprintf
+movr0, #2
+movr1, #0
+movr2, #0
+movr3, #0
+movr4, #8
+push{r4}
+blpoly3
+addsp, #4
+movr1, r0
+ldrr0, =var1
+blprintf
+pop{r4, lr}
+bxlr
+.equparam5, 4 *1 
+poly3:push{r4}
+ldr4, [sp, #param5] 
+smlabbr3, r2, r4, r3 
+smulbbr2, r4, r4 
+smlabbr3, r1, r2, r3 
+smulbbr2, r2, r4
+smlabbr0, r0, r2, r3 
+pop{r4}
+bxlr 
