6134861b6674e14a763e2a8e23694c57b692cfaf
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-06 19:05:37 -0800

    Update subrutina1en.c

--- Capitulo3/subrutina1en.c
+++ Capitulo3/subrutina1en.c
@@ -1,11 +1,24 @@
+/*Instito tecnológico de Tijuana
+* Lenguaje de interfaz
+* Autor: Rodriguez Moreno Omar Gamaliel
+* 06/12/2021
+*/
 #include <stdio.h>
 
 void main(void)
 {
   int i;
-  mysrand (42);
+  mysrand (42); //método random
   for ( i = 0; i <5; i ++ )
   {
-    printf ( " %d\n " , myrand ());
+    printf ( " %d\n " , myrand());
   }
+}
+static int seed;
+short myrand(void){
+  seed = seed *1103515245 + 12345;
+  return seed>>16 & 0x7fff ;
+}
+void mysrand(int x){
+  seed = x;
 }
