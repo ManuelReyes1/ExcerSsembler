a3cb15d86f776162e74a1dddd48947de61cb8afb
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-06 19:41:04 -0800

    Update EvalPoli.s

--- Capitulo3/EvalPoli.s
+++ Capitulo3/EvalPoli.s
@@ -2,25 +2,33 @@
 * Lenguaje de interfaz
 * Autor: Rodriguez Moreno Omar Gamaliel
 * 06/12/2021
+*   Evalúa x polinomio con esta estructura
+*   f(x) = ax3 + bx2 + cx + d
 */
+
 .data
-var1: .asciz  " %d\ n"
+var1: .asciz  "%d\ n"
+
 .text
 .global main
 
 main: push  {r4,lr}
-
-  mov r0, #1
+/*con registros llenamos los primeros parámetros*/
+  mov r0, #1 
   mov r1, #2
   mov r2, #3
   mov r3, #4
-  mov r4, #5
+  mov r4, #5 //el quinto lo llenamos por pila
   push  {r4}
-  bl  poly3
-  add sp, #4
+  bl  poly3 //llamamos a la función que evaluará el polinomio con
+  add sp, #4 // los números que llenamos (del 1 a 5)
   mov r1, r0
   ldr r0, =var1
-  bl  printf
+  bl  printf //imprimimos
+  
+  /*llenamos con otros valores y llamamos una vez más a la función ahora 
+  con los nuevos valores que le daremos*/
+  
   mov r0, #1
   mov r1, #-1
   mov r2, #1
@@ -28,10 +36,12 @@ main: push  {r4,lr}
   mov r4, #8
   push  {r4}
   bl  poly3
-  add sp, #4
+  add sp, #4 
   movr1, r0
   ldrr0, =var1
-  blprintf
+  blprintf //imprimimos segunda llamada ( 1,-1,1,-1,8)
+  
+  /*hacemos un tercer llamado al método*/
   mov r0, #2
   mov r1, #0
   mov r2, #0
@@ -42,16 +52,17 @@ main: push  {r4,lr}
   add sp, #4
   mov r1, r0
   ldr r0, =var1
-  bl  printf
+  bl  printf //imprimimos tercera llamada (2,0,0,0,8)
   pop {r4, lr}
   bx  lr
   .equ  param5, 4*1 
+  /*función/
 poly3:  push  {r4}
   ldr 4, [sp, #param5] 
-  smlabb  r3, r2, r4, r3 
-  smulbb  r2, r4, r4 
-  smlabb  r3, r1, r2, r3 
-  smulbb  r2, r2, r4
-  smlabb  r0, r0, r2, r3 
+  smlabb  r3, r2, r4, r3 //c*x+d
+  smulbb  r2, r4, r4 //x*x
+  smlabb  r3, r1, r2, r3 //b*(x*x) + (c*x + d)
+  smulbb  r2, r2, r4 //x*(x*x)
+  smlabb  r0, r0, r2, r3 //a*x*x*x + b*x*x + c*x+d
   pop {r4}
   bx  lr 
