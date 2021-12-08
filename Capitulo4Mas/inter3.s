277cb0fe0b79814c11f070fd58f9554845f2120f
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-08 00:47:59 -0800

    Update inter3.s

--- Capitulo4Mas/inter3.s
+++ Capitulo4Mas/inter3.s
@@ -1,19 +1,17 @@
 .include " inter.inc "
 .text
-/* Agrego vector interrupci ón */
+
 ADDEXC 0x18, irq_handler
-/* Inicializo la pila en modos IRQ y SVC */
-mov r0, # 0b11010010 @ Modo IRQ, FIQ & IRQ desact
+mov r0, # 0b11010010 
 msr cpsr_c, r0
 mov sp, # 0x8000
-mov r0, # 0b11010011 @ Modo SVC, FIQ & IRQ desact
+mov r0, # 0b11010011
 msr cpsr_c, r0
 mov sp, # 0x8000000
 /* Configuro GPIOs 9 y 10 como salida */
 ldr r0, = GPBASE
 mov r1, # 0b00001000000000000000000000000000
 str r1, [ r0, # GPFSEL0 ]
-/* guia bits xx999888777666555444333222111000 */
 mov r1, # 0b00000000000000000000000000000001
 str r1, [ r0, # GPFSEL1 ]
 /* Enciendo LEDs 10987654321098765432109876543210 */
@@ -23,16 +21,12 @@ str r1, [ r0, # GPSET0 ]
 mov r1, # 0b00000000000000000000000000001100
 str r1, [ r0, # GPFEN0 ]
 ldr r0, = INTBASE
-/* Habilito interrupciones, local y globalmente */
 mov r1, # 0b00000000000100000000000000000000
-/* guia bits 10987654321098765432109876543210 */
 str r1, [ r0, # INTENIRQ2 ]
-mov r0, # 0b01010011 @ Modo SVC, IRQ activo
+mov r0, # 0b01010011 
 msr cpsr_c, r0
-/* Repetir para siempre */
 bucle : b bucle
 
-/* Rutina de tratamiento de interrupci ón */
 irq_handler :
 push { r0, r1 }
 ldr r0, = GPBASE
@@ -46,8 +40,6 @@ ands r1, # 0b00000000000000000000000000000100
 movne r1, # 0b00000000000000000000001000000000
 moveq r1, # 0b00000000000000000000010000000000
 str r1, [ r0, # GPSET0 ]
-/* Desactivo los dos flags GPIO pendientes de atenci ón
-guia bits 54321098765432109876543210 */
 mov r1, # 0b00000000000000000000000000001100
 str r1, [ r0, # GPEDS0 ]
 pop { r0, r1 }
