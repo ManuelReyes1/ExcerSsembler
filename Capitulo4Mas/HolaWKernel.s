773a80bd241e9636e55d9fb73640804dc7775a6d
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-07 23:44:01 -0800

    Update HolaWKernel.s

--- Capitulo4Mas/HolaWKernel.s
+++ Capitulo4Mas/HolaWKernel.s
@@ -1,14 +1,23 @@
+/*Instito tecnológico de Tijuana
+* Lenguaje de interfaz
+* Autor: Rodriguez Moreno Omar Gamaliel
+* 10/12/2021
+*/
+
 .data
-cadena : .asciz " Hola Mundo !\ n "
-cadenafin :
+
+cadena: .asciz  "Hola Mundo! \n"
+cadenafin:
+
 .text
 .global main
-main : push { r7, lr } /* preservamos reg. */
-mov r0, # 1 /* salida est á ndar */
-ldr r1, = cadena /* cadena a enviar */
-mov r2, # cadenafin - cadena /* longitud */
-mov r7, # 4 /* seleccionamos la */
-swi #0 /* llamada a sistema ’write ’*/
-mov r0, # 0 /* devolvemos ok */
-pop { r7, lr } /* recuperamos reg. */
-bx lr /* salimos de main */
+
+main: push  {r7, lr} 
+  mov r0, #1
+  ldr r1, =cadena //a r1 le damos cadena
+  mov r2, #cadenafin-cadena //generamos la longitud
+  mov r7, #4 
+  swi #0 
+  mov r0, #0 
+  pop {r7, lr} 
+  bx  lr //salimos
