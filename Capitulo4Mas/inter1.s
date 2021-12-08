8d3108465ae0a0c32f6582b6488e9ffa720e84c5
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-08 00:18:18 -0800

    Update and rename inter.s to inter1.s

---
+++ Capitulo4Mas/inter1.s
@@ -0,0 +1,42 @@
+/*Instito tecnológico de Tijuana
+* Lenguaje de interfaz
+* Autor: Rodriguez Moreno Omar Gamaliel
+* 10/12/2021
+* Encender un led rojo después de 4 segundos
+*/
+  .include  "inter.inc"
+.text
+  ADDEXC 0x18, irq_handler
+
+  mov r0, #0b11010010
+  msr cpsr_c, r0
+  mov sp, #0x8000
+  mov r0, #0b11010011 
+  msr cpsr_c, r0
+  mov sp, #0x8000000
+
+/* Asignamos el pin GPIO9 como salida*/
+  ldr r0, =GPBASE
+  mov r1, #0b00001000000000000000000000000000
+  str r1, [r0, #GPFSEL0]
+
+  ldr r0, =STBASE
+  ldr r1, [r0, #STCLO]
+  add r1, #0x400000 //agregamos los 4.19 segundos
+  str r1, [r0, #STC1]
+
+  ldr r0, =INTBASE
+  mov r1, #0b0010
+  str r1, [r0, #INTENIRQ1]
+  mov r0, #0b01010011 
+  msr cpsr_c, r0
+
+bucle:  b bucle //orden del bucle sin fin
+/*tratamos la interrupción*/
+irq_handler:
+  push  {r0, r1}
+  ldr r0, =GPBASE
+  mov r1, #0b00000000000000000000001000000000
+  str r1, [r0, #GPSET0] //aquí se enciende el led
+  pop {r0, r1} 
+  subs  pc, lr, #4 
